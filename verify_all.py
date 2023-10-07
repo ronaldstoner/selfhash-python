901076531c30098d6595d2d4a33f79798ee910d8
Ron Stoner <ron@team.casa>
2023-10-06 18:52:56 -0600

    Linting fixes

--- verify_all.py
+++ verify_all.py
@@ -3,11 +3,11 @@
 
 """Script to verify all python files in a directory recursively"""
 
-import os
 import glob
 import selfhash
 
 def hash_and_verify_files(directory):
+    """Hash and verify all found .py files"""
     # Use glob to find all .py files in the directory and its subdirectories
     for filename in glob.iglob(directory + '**/*.py', recursive=True):
         print(f"Processing {filename}...")
@@ -15,7 +15,7 @@ def hash_and_verify_files(directory):
         # Instantiate a new selfhash class for each file
         hasher = selfhash.SelfHash()
         hasher.hash(filename)
-        
+
         if hasher.known_hash == "INSERT_HASH_HERE":
             print(f"Generated Hash for {filename}: {hasher.source_code_hash}")
             print("Please replace INSERT_HASH_HERE with this hash and run the script again.")
