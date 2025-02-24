5df7929ef6f6b6c44bf262182d5e1df20c8bfb48
Ron Stoner <ron@botanixlabs.xyz>
2025-02-24 15:31:16 -0700

    linter fixes

--- selfhash/__init__.py
+++ selfhash/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-# Hash: 2b5837c63ddf7dfe377db36b244d0a9fb3d5e44dcaa98da7cb0a4da66645925d
+# Hash: 3db4cafd1f7d5f3ea9ef93ef745ad8c7cd6561619ba1fe150bae61d5c7ed78dc
 # Author: Ron Stoner
 # Github: ronaldstoner
 # Website: stoner.com
 
+"""
+SelfHash init
+"""
+
 from .selfhash import SelfHash
-
-
