98674e0e2ff8a4731cdeaeace3362d24a0aa4c1c
Ron Stoner <ron@botanixlabs.xyz>
2025-02-24 15:25:59 -0700

    Add missing __init.py

---
+++ selfhash/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Hash: 2b5837c63ddf7dfe377db36b244d0a9fb3d5e44dcaa98da7cb0a4da66645925d
+# Author: Ron Stoner
+# Github: ronaldstoner
+# Website: stoner.com
+
+from .selfhash import SelfHash
+
+
